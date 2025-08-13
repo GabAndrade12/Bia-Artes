57427b905c40f106dfd7f82a9eaba306178e193d
Gabriel Andrade <g.andrade1907@gmail.com>
2025-08-13 16:46:11 -0300

    Home criada

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>Bia Artes</h1>
+      <ul>
+        <li><a href="#">Início</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Produtos</a></li>
+        <li><a href="#">Contato</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <h2>Bem-vindo(a) à Bia Artes!</h2>
+        <p>Seus produtos de crochê favoritos estão aqui.</p>
+      </main>
+    </>
+  );
+}
+
+export default Home;
